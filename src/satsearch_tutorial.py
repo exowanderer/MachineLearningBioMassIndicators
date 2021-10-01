4d0f21e2110da901858373c065f2f3a27141df3c
Jonathan Fraine <jdfraine@gmail.com>
2021-10-01 22:36:36 +0200

    satsearch Search criteria successful from tutorial

---
+++ src/satsearch_tutorial.py
@@ -0,0 +1,28 @@
+
+from satsearch import Search
+
+url_earth_search = 'https://earth-search.aws.element84.com/v0'
+
+eo_bbox = [-110, 39.5, -105, 40.5]
+eo_datetime = '2018-02-12T00:00:00Z/2018-03-18T12:31:12Z'
+eo_query = {
+    'eo:cloud_cover': {'lt': 10}
+}
+
+search = Search(
+    url=url_earth_search,
+    bbox=eo_bbox
+)
+print(f'bbox search: {search.found()} items')
+
+search = Search(
+    url=url_earth_search,
+    datetime=eo_datetime
+)
+print(f'datetime search: {search.found()} items')
+
+search = Search(
+    url=url_earth_search,
+    query=eo_query
+)
+print(f'eo:cloud_cover search: {search.found()} items')
