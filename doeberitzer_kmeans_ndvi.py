167f565c5ce1f9863f39ef549d45e34d63e637a0
Jonathan Fraine <jdfraine@gmail.com>
2021-10-07 17:36:48 +0200

    fixed formatting in run file

--- doeberitzer_kmeans_ndvi.py
+++ doeberitzer_kmeans_ndvi.py
@@ -2,17 +2,11 @@ from argparse import ArgumentParser
 from dotenv import load_dotenv
 from matplotlib import pyplot as plt
 
-# TODO: change from utils to .utils when modularizing
-from kmeans_ndvi import (
-    KMeansNDVI,
-    info_message,
-    warning_message,
-    debug_message
-)
+from kmeans_ndvi import KMeansNDVI, info_message, warning_message, debug_message
 
 
-if __name__ == '__main__':
-    example_usage = """
+if __name__ == "__main__":
+    EXAMPLE_USAGE = """
     Use case:
 
     python doeberitzer_kmeans_ndvi.py \
@@ -29,24 +23,26 @@ if __name__ == '__main__':
     python doeberitzer_kmeans_ndvi.py --band_names b04 b08 --start_date 2020-01-01 --end_date 2020-02-01 --cloud_cover 1 --download --verbose --verbose_plot
     """
 
-    args = ArgumentParser(prog='Doeberitzer K-Means NDVI')
+    args = ArgumentParser(prog="Doeberitzer K-Means NDVI")
     args.add_argument(
-        '--geojson', type=str, default='doeberitzer_multipolygon.geojson'
+        "--geojson",
+        type=str,
+        default="doeberitzer_multipolygon.geojson"
     )
-    args.add_argument('--scene_id', type=str)
-    args.add_argument('--band_names', nargs='+', default=['B04', 'B08'])
-    args.add_argument('--collection', type=str, default='sentinel-s2-l2a')
-    args.add_argument('--start_date', type=str, default='2020-01-01')
-    args.add_argument('--end_date', type=str, default='2020-02-01')
-    args.add_argument('--cloud_cover', type=int, default=1)
-    args.add_argument('--n_sig', type=float, default=10)
-    args.add_argument('--download', action='store_true')
-    args.add_argument('--env_filename', type=str, default='.env')
-    args.add_argument('--n_clusters', type=int, default=5)
-    args.add_argument('--quantile_range', nargs='+', default=(1, 99))
-    args.add_argument('--verbose', action='store_true')
-    args.add_argument('--verbose_plot', action='store_true')
-    args.add_argument('--quiet', action='store_true')
+    args.add_argument("--scene_id", type=str)
+    args.add_argument("--band_names", nargs="+", default=["B04", "B08"])
+    args.add_argument("--collection", type=str, default="sentinel-s2-l2a")
+    args.add_argument("--start_date", type=str, default="2020-01-01")
+    args.add_argument("--end_date", type=str, default="2020-02-01")
+    args.add_argument("--cloud_cover", type=int, default=1)
+    args.add_argument("--n_sig", type=float, default=10)
+    args.add_argument("--download", action="store_true")
+    args.add_argument("--env_filename", type=str, default=".env")
+    args.add_argument("--n_clusters", type=int, default=5)
+    args.add_argument("--quantile_range", nargs="+", default=(1, 99))
+    args.add_argument("--verbose", action="store_true")
+    args.add_argument("--verbose_plot", action="store_true")
+    args.add_argument("--quiet", action="store_true")
     clargs = args.parse_args()
 
     load_dotenv(clargs.env_filename)
@@ -65,7 +61,7 @@ if __name__ == '__main__':
         quantile_range=clargs.quantile_range,
         verbose=clargs.verbose,
         verbose_plot=clargs.verbose_plot,
-        quiet=clargs.quiet
+        quiet=clargs.quiet,
     )
 
     info_message(jp2_data)
