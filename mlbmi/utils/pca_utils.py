f3c6dd5cdbc763786fd4d9728ad119456e4a91da
jonathan fraine <jdfraine@gmail.com>
2021-11-12 23:45:03 +0100

    initial PCA operations. no more erroros. still bugs

--- mlbmi/utils/pca_utils.py
+++ mlbmi/utils/pca_utils.py
@@ -3,7 +3,13 @@ from matplotlib import pyplot as plt
 from sklearn.decomposition import PCA, MiniBatchSparsePCA
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
+from mlbmi.utils.base_utils import debug_message, warning_message, info_message
 
+def determine_n_components_extra(image_size, n_components):
+    for n_comp_extra in range(image_size):
+        n_samples_ = image_size / (n_components + n_comp_extra)
+        if int(n_samples_) == n_samples_:
+            return n_comp_extra
 
 def pca_spatial_components(
         image, n_components=5, quantile_range=(1, 99), whiten=True,
@@ -19,14 +25,16 @@ def pca_spatial_components(
     Returns:
         sklearn.components._pca.PCA: trained pca components object
     """
+
     # Scale the input image data after reformatting 2D image as 1D vector and
     #   rejecting the outliers using the RobustScaler algorithm
+    n_extra = determine_n_components_extra(image.size, n_components)
     sclr = RobustScaler(quantile_range=quantile_range)
-    pixel_scaled = sclr.fit_transform(image.reshape(-1, 1))
+    pixel_scaled = sclr.fit_transform(image.reshape(-1, n_components + n_extra))
 
     # Configure pca components instance
     pca = PCA(
-        n_components=n_components,
+        n_components=n_components + n_extra,
         copy=True,
         whiten=whiten,
         svd_solver='auto',
@@ -36,7 +44,7 @@ def pca_spatial_components(
     )
 
     # Compute the K-Means components and store in object
-    pca.fit(pixel_scaled)
+    pca.fit(pixel_scaled.reshape(-1, n_components + n_extra))
 
     if verbose_plot:
         # Show NDVI components images
@@ -65,17 +73,24 @@ def pca_temporal_components(
 
     # Preprocess image data into a sequence of (nonzero) pixels over time
     samples_ = image_stack.reshape(image_stack.shape[0], -1).T
-    where_zero = samples_.sum(axis=1) == 0
-    samples_ = samples_[~where_zero]
+    # where_zero = samples_.sum(axis=1) == 0
+    # samples_ = samples_[~(samples_.sum(axis=1) == 0)]
 
     # Scale the input image data after reformatting 2D image as 1D vector and
     #   rejecting the outliers using the RobustScaler algorithm
     sclr = RobustScaler(quantile_range=quantile_range)
     samples_scaled = sclr.fit_transform(samples_)
 
+    n_components_ = np.min([n_components, image_stack.shape[0]])
+    if n_components_ != n_components:
+        warning_message(
+            "n_components cannot be larger than the number of time stamps.\n"
+            f"Modified n_components from {n_components} to {n_components_}."
+        )
+
     # Configure pca components instance
     pca = PCA(
-        n_components=n_components,
+        n_components=n_components_,
         copy=True,
         whiten=whiten,
         svd_solver='auto',
@@ -146,9 +161,10 @@ def sanity_check_ndvi_statistics(
         plt.show()
 
 
-def sanity_check_spatial_pca(pca, image, quantile_range=(1, 99),
-                             scene_id=None, res=None, date=None,
-                             plot_now=False):
+def sanity_check_spatial_pca(
+    pca, image, quantile_range=(1, 99),
+    scene_id=None, res=None, date=None,
+    plot_now=False):
     """Plot imshow of components solution as sanity check
 
     Args:
@@ -158,25 +174,31 @@ def sanity_check_spatial_pca(pca, image, quantile_range=(1, 99),
             threshold. Defaults to (1, 99).
     """
     # Preprocess image data
+    # n_extra = determine_n_components_extra(image.size, pca.n_components)
     sclr = RobustScaler(quantile_range=quantile_range)
-    pixel_scaled = sclr.fit_transform(image.reshape(-1, 1))
+    pixel_scaled = sclr.fit_transform(
+        image.reshape(-1, pca.n_components)
+    )
 
     # Predict each components value per pixel
-    components_pred = pca.predict(pixel_scaled)
+    components_pred = pca.transform(pixel_scaled)
 
     base_fig_size = 5  # Each sub figure will be base_fig_size x base_fig_size
     fig, axs = plt.subplots(
         ncols=pca.n_components + 1,
-        figsize=(base_fig_size * (pca.n_components + 1), base_fig_size)
+        figsize=(base_fig_size * (pca.n_components + 1), base_fig_size),
+        sharex=True,
+        sharey=True
     )
 
     # Plot the entire components_pred image
     axs[0].imshow(components_pred.reshape(image.shape), interpolation='None')
 
     # Cycle through and plot each components_pred image per 'class'
-    for k in range(pca.n_components):
+    for k, comp_ in enumerate(pca.components_):
+        print((components_pred == comp_).sum())
         axs[k + 1].imshow(
-            (components_pred == k).reshape(image.shape),
+            (components_pred == comp_).T.reshape(image.shape),
             interpolation='None'
         )
 
@@ -217,43 +239,47 @@ def sanity_check_temporal_pca(
             threshold. Defaults to (1, 99).
     """
     samples_ = image_stack.reshape(image_stack.shape[0], -1).T
-    where_zero = samples_.sum(axis=1) == 0
-    samples_notzero = samples_[~where_zero]
+    # where_zero = samples_.sum(axis=1) == 0
+    # samples_notzero = samples_[~where_zero]
 
     # Scale the input image data after reformatting 2D image as 1D vector and
     #   rejecting the outliers using the RobustScaler algorithm
     sclr = RobustScaler(quantile_range=quantile_range)
-    samples_scaled = sclr.fit_transform(samples_notzero)
+    # samples_scaled = sclr.fit_transform(samples_notzero)
+    samples_scaled = sclr.fit_transform(samples_)
 
     # Predict each components value per pixel
-    components_pred = pca.predict(samples_scaled)
+    components_pred = pca.transform(samples_scaled)
 
     # Embedd above image in a zero array to re-constitute zeros
     #   for the out of mask shape
-    components_image = np.zeros(samples_.shape[0])
+    # components_image = np.zeros(samples_.shape[0])
+    image_shape = list(image_stack.shape[1:])
+    image_shape.append(pca.n_components)
+    components_image = components_pred.reshape(image_shape)
 
     # Add one to each Class to represent the "out of mask" is class zero
-    components_image[~where_zero] = components_pred + 1
+    # components_image[~where_zero] = components_pred + 1
 
     # Reshape 1D array into 2D image of the original image shape
-    img_shape = image_stack.shape[1:]
-    components_image = components_image.reshape(img_shape)
+    # img_shape = image_stack.shape[1:]
+    # components_image = components_image.reshape(img_shape)
 
     base_fig_size = 5  # Each sub figure will be base_fig_size x base_fig_size
     fig, axs = plt.subplots(
-        ncols=pca.n_components + 2,
+        ncols=pca.n_components,
         figsize=(base_fig_size * (pca.n_components + 1), base_fig_size)
     )
 
-    # Plot the entire components_pred image
-    axs[0].imshow(components_image, interpolation='None')
+    # # Plot the entire components_pred image
+    # axs[0].imshow(components_image, interpolation='None')
 
     # Plot the pixels outside the mask, which were not componentsed
-    axs[1].imshow(components_image == 0, interpolation='None')
+    # axs[1].imshow(components_image == 0, interpolation='None')
 
     # Cycle through and plot each components_pred image per 'class'
     for k in range(pca.n_components):
-        axs[k + 2].imshow((components_image == (k + 1)), interpolation='None')
+        axs[k].imshow(components_image[:, :, k], interpolation='None')
 
     # Remove all unnecessary markers from figure
     [ax.grid(False) for ax in axs.ravel()]  # remove grid for images
